cd91191ae0d7ff2fb08684dd0a649aaa29c509e2
aashoksudani <ashok.sudani@gatewaytechnolabs.com>
2015-12-02 14:45:56 +0530

    second checkin

--- DemoGitHub/DemoGitHub/Global.asax.cs
+++ DemoGitHub/DemoGitHub/Global.asax.cs
@@ -15,6 +15,7 @@ namespace DemoGitHub
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
